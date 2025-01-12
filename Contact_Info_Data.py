f76791e2b362b93a66fcb116a1f6f88243b1fd95
snoopboguno <bwarisha1@gmail.com>
2025-01-12 11:12:08 -0500

    Function to get contact info

---
+++ Contact_Info_Data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+# Path to your Excel file
+Contact_Info_Data_File = fr"{os.curdir}\resources\PWS_Contact_Info_Cleaned.xlsx"
+
+
+# Read the Excel file
+
+
+#Display the contents of the Dataframe
+def Get_Contact_Info_By_Water_System (pwsid):
+    dataframe = pd.read_excel(Contact_Info_Data_File)
+    for i in range (len(dataframe)):
+        row=dataframe.iloc[i]
+        if str (row["PWSID"])==pwsid:
+            return row
+
+
+if __name__=="__main__":
+    result=Get_Contact_Info_By_Water_System("102001")
+    print (result)
