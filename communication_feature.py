4a289518ffe103addfb48556a9d45c4a924c0e7f
JosephQV <112574271+JosephQV@users.noreply.github.com>
2025-01-16 21:40:04 -0500

    Setup email form screen

--- communication_feature.py
+++ communication_feature.py
@@ -1,6 +1,8 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 
 
 # This class is the main screen reached when navigating to a "communication" or "feedback"
@@ -16,6 +18,11 @@ class FeedbackNavigationMenu(BoxLayout):
         super(FeedbackNavigationMenu, self).__init__(**kwargs)
         
 
+class FeedbackMenuItem(Button):
+    item_text = StringProperty("")          # text displayed for this option
+    screen_selection = ""   # screen name to navigate to when clicked
+        
+
 # These classes represent Screens that would be navigated to from the communication
 # screen for different topics of feedback. For now there is just this one as a
 # placeholder / example.
@@ -23,4 +30,15 @@ class TapWaterFeedbackScreen(Screen):
     pass
 
     
+class EmailScreen(Screen):
+    recipient = "default recipient (placeholder)"
+    email_body_text = StringProperty("Default email body (placeholder)")
     
+    
+    def test_submit(self):
+        # placeholder function to demonstrate calling a function upon clicking a
+        # button in the form
+        # print attribute values
+        print(self.recipient)
+        print(self.email_body_text)
+        self.email_body_text = "Submitted"
